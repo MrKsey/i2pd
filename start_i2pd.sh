c66a988609b77fad097174f21be42f40135c9115
ksey <ksey3000@gmail.com>
2017-11-23 23:48:07 +0300

    Add files via upload

---
+++ start_i2pd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -e /var/lib/i2pd/certificates ]; then
+  cp -R /usr/share/i2pd/certificates /var/lib/i2pd/certificates
+fi
+
+if [ ! -e /var/lib/i2pd/i2pd.conf ]; then
+  cp /etc/i2pd/i2pd.conf /var/lib/i2pd/i2pd.conf
+fi
+
+if [ ! -e /var/lib/i2pd/subscriptions.txt ]; then
+  cp /etc/i2pd/subscriptions.txt /var/lib/i2pd/subscriptions.txt
+fi
+
+if [ ! -e /var/lib/i2pd/tunnels.conf ]; then
+  cp /etc/i2pd/tunnels.conf /var/lib/i2pd/tunnels.conf
+fi
+
+i2pd --service $PARAMS
